4bf8856d80c80caf8d0a98be0b6967d724e8f023
Serge Koval <serge@vectorops.dev>
2025-06-13 22:54:15 -0400

    feat: add parse_path function for recursive file parsing implementation

--- know/parsers.py
+++ know/parsers.py
@@ -24,3 +24,11 @@ class AbstractCodeParser(ABC):
             FileMetadata: The parsed file metadata.
         """
         pass
+
+
+def parse_path(project):
+    # TODO: Implement a function that recursively goes through files of the RepoMetadata
+    # For each file, call concrete implementation of the file parsing function. Create a singleton helper class that maps file extensions to concrete CodeParser implementations.
+    # Parsers will return partially filled FileMetadata with all nested structures. We will want to create or update or delete records in the database by using corresponding repositories for FileMetadata and SymbolMetadata.
+    # For FileMetadata check if file_hash has changed. If it did not - skip the file.
+    # For SymbolMetadata, check if symbol_hash has changed. If it is - we will need to reclculate various SymbolMetadata via new "populate_symbol_meta" function before saving symbols recursively.
